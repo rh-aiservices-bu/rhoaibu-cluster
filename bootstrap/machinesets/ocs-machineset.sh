4533269afb9f5bedd5da56078ec52206778f177c
rcarrata <rcarratalasanchez@gmail.com>
2026-07-15 10:44:12 +0200

    Revert "add explicit disk config to ocs-machineset script (gp3, 500GB, 3000 IOPS, 250 throughput)"
    This reverts commit 3d2d86dd31b1b29e5df838e7800fcef996f7aa02.

--- bootstrap/machinesets/ocs-machineset.sh
+++ bootstrap/machinesets/ocs-machineset.sh
@@ -11,13 +11,9 @@ fi
 for MACHINESET in $(oc get -n openshift-machine-api machinesets -o name | grep -v ocs )
 do
   oc get -n openshift-machine-api "$MACHINESET" -o json | jq '
-      del( .metadata.uid, .metadata.managedFields, .metadata.selfLink, .metadata.resourceVersion, .metadata.creationTimestamp, .metadata.generation, .status) |
-      (.metadata.name, .spec.selector.matchLabels["machine.openshift.io/cluster-api-machineset"], .spec.template.metadata.labels["machine.openshift.io/cluster-api-machineset"]) |= sub("worker";"workerocs") |
+      del( .metadata.uid, .metadata.managedFields, .metadata.selfLink, .metadata.resourceVersion, .metadata.creationTimestamp, .metadata.generation, .status) | 
+      (.metadata.name, .spec.selector.matchLabels["machine.openshift.io/cluster-api-machineset"], .spec.template.metadata.labels["machine.openshift.io/cluster-api-machineset"]) |= sub("worker";"workerocs") | 
       (.spec.template.spec.providerSpec.value.instanceType) |= "m6i.4xlarge" |
-      (.spec.template.spec.providerSpec.value.blockDevices[0].ebs.volumeType) |= "gp3" |
-      (.spec.template.spec.providerSpec.value.blockDevices[0].ebs.volumeSize) |= 500 |
-      (.spec.template.spec.providerSpec.value.blockDevices[0].ebs.iops) |= 3000 |
-      (.spec.template.spec.providerSpec.value.blockDevices[0].ebs.throughputMib) |= 250 |
       (.spec.template.spec.metadata.labels["cluster.ocs.openshift.io/openshift-storage"]) |= ""' | oc apply -f -
 done
 
